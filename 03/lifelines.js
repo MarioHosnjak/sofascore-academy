9477cabb9d2a4c2918e8ae414573597ba7b2bb12
Mario Hosnjak <mario.hosnjak009@gmail.com>
2024-03-15 22:57:07 +0100

    Added phone-a-friend lifeline

--- 03/lifelines.js
+++ 03/lifelines.js
@@ -1,18 +1,9 @@
+//          50:50
 function fifty_fifty() {
     document.getElementById("lifeline-button-1").disabled = true
     let wrongAnswers = questions.results[currentQuestionNumber].incorrect_answers
     shuffleArray(wrongAnswers)
     disableWrongAnswers(wrongAnswers[0], wrongAnswers[1])
-}
-
-function phone_a_friend() {
-    alert("zovi ćovika")
-    document.getElementById("lifeline-button-2").disabled = true
-}
-
-function ask_the_audience() {
-    alert("pitaj publiku")
-    document.getElementById("lifeline-button-3").disabled = true
 }
 
 function disableWrongAnswers(wrongAnswer1, wrongAnswer2) {
@@ -29,6 +20,30 @@ function enableAnswerButtons() {
     for (let i = 0; i < 4; i++) {
         buttons[i].disabled = false
     }
+}
+
+//          phone a friend
+
+function phone_a_friend() {
+    document.getElementById("lifeline-button-2").disabled = true
+    let popUpDiv = document.getElementById("phone_a_friend_pop_up")
+    popUpDiv.style.display = "block"
+    setTimeout(() => {
+        let rightAnswer = questions.results[currentQuestionNumber].correct_answer
+        document.getElementById("right_answer_p").innerHTML = "I think it's: " + rightAnswer
+    }, 2000)
+}
+
+function close_lifeline_popup() {
+    document.getElementById("phone_a_friend_pop_up").style.display = "none"
+    document.getElementById("right_answer_p").innerHTML = ""
+}
+
+//          ask the audience
+
+function ask_the_audience() {
+    alert("pitaj publiku")
+    document.getElementById("lifeline-button-3").disabled = true
 }
 
 function enableLifelineButtons() {
