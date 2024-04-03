603145474d69ca7077a013deeeadf9924136b4b1
Mario Hosnjak <mario.hosnjak009@gmail.com>
2024-04-03 17:36:51 +0200

    Added pokemon full view

--- 04-pokedex/src/components/PokemonDetails.tsx
+++ 04-pokedex/src/components/PokemonDetails.tsx
@@ -18,15 +18,24 @@ export const PokemonDetails = ({pokemonInfo}: {pokemonInfo:pokemonDetails|undefi
 
     return (
         <>
-            <div className="pokemon-details-div">
-                <a className="pokemon-details-name">
-                    #{String(pokemonInfo?.id).padStart(4, '0')} {pokemonInfo && pokemonInfo.name.charAt(0).toUpperCase() + pokemonInfo.name.slice(1).toLowerCase()}
-                </a>
-                <a className="pokemon-details-a">Health points: <span>{pokemonInfo?.healthPoints} HP</span></a>
-                <a className="pokemon-details-a">Height: <span>{pokemonInfo?.height} cm</span></a>
-                <a className="pokemon-details-a">Weight: <span>{pokemonInfo?.weight} kg</span></a>
-                <a className="pokemon-details-a">Type: <span>{pokemonInfo?.type.map((v)=>{return v + " "})}</span></a>
-                <a className="pokemon-details-a">Details: <span>{pokemonInfo?.details}</span></a>
+            <div className="pokemon-details-container">
+                <div className="pokemon-details-div">
+                    <a className="pokemon-details-name">
+                        #{String(pokemonInfo?.id).padStart(4, '0')} {pokemonInfo && pokemonInfo.name.charAt(0).toUpperCase() + pokemonInfo.name.slice(1).toLowerCase()}
+                    </a>
+                    <a className="pokemon-details-a">Health points: <span>{pokemonInfo?.healthPoints} HP</span></a>
+                    <a className="pokemon-details-a">Height: <span>{pokemonInfo?.height} cm</span></a>
+                    <a className="pokemon-details-a">Weight: <span>{pokemonInfo?.weight} kg</span></a>
+                    <a className="pokemon-details-a">Type: <span>{pokemonInfo?.type.map((v)=>{return v + " "})}</span></a>
+                    <a className="pokemon-details-a">Details: <span>{pokemonInfo?.details}</span></a>
+                </div>
+                <div style={{display: "flex", flexDirection: "column", justifyContent: "center"}}>
+                    <a className="pokemon-details-a">Full view: </a>
+                    <div style={{display: "flex", flexDirection: "row", alignItems: "center"}}>
+                        <img style={{height: "120px", width: "120px"}} src={pokemonInfo?.viewFront} alt="Front View"></img>
+                        <img style={{height: "120px", width: "120px"}} src={pokemonInfo?.viewBack} alt="Back View"></img>
+                    </div>
+                </div>
             </div>
         </>
     )
